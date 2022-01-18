c2ce0c8d45b8c2f69019aae4ce8751d15f5c96a6
juhwano <calculus9006@gmail.com>
2022-01-18 18:29:51 +0900

    ✨main test 질문응답페이지 완료

--- common/views.py
+++ common/views.py
@@ -4,10 +4,4 @@ from django.shortcuts import render, redirect
 from .forms import UserForm
 
 def index(request):
-    return HttpResponse('<h1>프로젝트 메인 테스트</h1><br/>'
-                        '<a href="index">홈페이지 메인</a><br/>'
-                        '<a href="talk">직업 게시판</a><br/>'
-                        '<a href="users">회원 관리</a><br/>'
-                        '<a href="discover">직업 탐색</a><br/>'
-                        '<a href="admin">관리자 페이지</a><br/>'
-                        )
+    return render(request, 'common/index.html')
